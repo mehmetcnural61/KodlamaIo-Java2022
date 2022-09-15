20bc95e778c605ed49eb6d70d55dc014464043ec
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-15 22:19:04 +0300

    Loops

---
+++ src/FirstDayOdev/LoopFor/Main.java
@@ -0,0 +1,12 @@
+package FirstDayOdev.LoopFor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //For
+        for (int i=1; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Dongu Bitti !!");
+    }
+}
---
+++ src/FirstDayOdev/LoopWhile/Main.java
@@ -0,0 +1,13 @@
+package FirstDayOdev.LoopWhile;
+
+public class Main {
+    public static void main(String[] args) {
+        //While
+        int i=1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While Dongusu Sona Erdi..");
+    }
+}
