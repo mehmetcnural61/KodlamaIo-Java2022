c1eac0b03479c067295859d62fb251d34e11c175
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-24 00:44:46 +0300

    ReCap Demo Classes

---
+++ src/SecondDayOdev/ReCapDemoClasses/Main.java
@@ -0,0 +1,16 @@
+package SecondDayOdev.ReCapDemoClasses;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DortIslem dortIslem=new DortIslem();
+        int sonuc= dortIslem.Topla(3,4);
+        System.out.println("İşlem sonucu: "+sonuc);
+
+
+
+    }
+}
