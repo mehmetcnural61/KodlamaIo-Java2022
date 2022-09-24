446b8e07cd4c46e3a785fd43ce7c32a1eda256f0
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-24 14:26:29 +0300

    Getter & Setter

---
+++ src/SecondDayOdev/GetterSetter/ProductManager.java
@@ -0,0 +1,9 @@
+package SecondDayOdev.GetterSetter;
+
+public class ProductManager {
+
+    public void Add(Product product){
+        //JDBC
+        System.out.println("Ürün eklendi: "+product.getName());
+    }
+}
