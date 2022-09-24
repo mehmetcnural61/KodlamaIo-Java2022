aff469533655460a733f0b711e03921296493d04
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-24 15:12:50 +0300

    Method Overloading

---
+++ src/SecondDayOdev/MethodOverloading/DortIslem.java
@@ -0,0 +1,14 @@
+package SecondDayOdev.MethodOverloading;
+
+public class DortIslem {
+
+    //method parametrelerin imzaların farklı olması np
+    public int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+    public int topla(int sayi1, int sayi2,int sayi3) {
+        return sayi1 + sayi2+sayi3;
+    }
+
+
+}
