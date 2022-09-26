9ee34154bbe174f9197163d81d955c60241a460e
mehmetcnural61 <mehmetnural61@hotmail.com>
2022-09-26 11:44:29 +0300

    Dort İslem Update

--- src/SecondDayOdev/ReCapDemoClasses/DortIslem.java
+++ src/SecondDayOdev/ReCapDemoClasses/DortIslem.java
@@ -2,16 +2,17 @@ package SecondDayOdev.ReCapDemoClasses;
 
 public class DortIslem {
 
-    public int Topla(int sayi1, int sayi2) {
-        return sayi1 + sayi2;
+    public int topla(int n1, int n2) {
+        return n1 + n2;
     }
-    public int Cikar(int sayi1, int sayi2) {
-        return sayi1 - sayi2;
+    public int cikar(int n1, int n2) {
+        return n1 - n2;
     }
-    public int Carp(int sayi1, int sayi2) {
-        return sayi1 * sayi2;
+    public int carp(int n1, int n2) {
+        return n1 * n2;
     }
-    public int Bol(int sayi1, int sayi2) {
-        return sayi1 / sayi2;
+    public int bol(int n1, int n2) {
+        return n1 / n2;
     }
+
 }
--- src/SecondDayOdev/ReCapDemoClasses/Main.java
+++ src/SecondDayOdev/ReCapDemoClasses/Main.java
@@ -6,9 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int  n1,n2,select;
+        Scanner input=new Scanner(System.in);
+
         DortIslem dortIslem=new DortIslem();
-        int sonuc= dortIslem.Topla(3,4);
-        System.out.println("İşlem sonucu: "+sonuc);
+
+        System.out.println("1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
+        System.out.print("Seciminiz ?: ");
+        select=input.nextInt();
+
+
+        System.out.print("1.sayiyi giriniz: ");
+        n1=input.nextInt();
+        System.out.print("2.sayiyi giriniz: ");
+        n2=input.nextInt();
+
+
+        if(select ==1){
+            System.out.println("Toplam Sonuc: "+ (n1+n2));
+        } else if (select ==2) {
+            System.out.println("Cikarma Sonuc: "+ (n1-n2));
+        } else if (select==3) {
+            System.out.println("Carpma Sonuc: "+ (n1*n2));
+        }else if (select==4) {
+            if(n2!=0){
+                System.out.println("Bolme Sonuc: "+ (n1/n2));
+            }else
+                System.out.println("Bir Sayi 0'a bolunemez !");
+        }else {
+            System.out.println("Hatali Giris Yapildi !! Tekrar Deneyiniz");
+        }
 
 
 
